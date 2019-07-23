3c26610c41a364305347e6616a3159dbafa74c8e
Натали <t.natalie14@gmail.com>
2019-07-23 16:06:49 +0400

    поправила переменные в модуле перетаскивания

--- js/drag-n-drop.js
+++ js/drag-n-drop.js
@@ -1,16 +1,20 @@
 'use strict';
 (function () {
-  var PIN_Y_MIN = 130; // минимальная координата позиции метки по Y
-  var PIN_Y_MAX = 630; // максимальная координата позиции метки по Y
   var ClassName = {
     MAP_FADED: 'map--faded'
   };
   var Selector = {
     MAP: '.map',
     MAIN_PIN: '.map__pin--main',
-
     FORM: '.ad-form',
     ADDRESS: '#address'
+  };
+  var PinData = {
+    Y_MIN: 130,
+    Y_MAX: 630,
+    WIDTH: 65,
+    HEIGHT: 65,
+    MOVE_STEP: 10
   };
   var code = {
     LEFT: 37,
@@ -18,13 +22,11 @@
     RIGHT: 39,
     DOWN: 40,
     A: 65,
-    W: 88,
+    W: 87,
     D: 68,
     S: 83,
     TAB: 9
   };
-  var MOVE_STEP = 5;
-
   var map = document.querySelector(Selector.MAP); // блок с картой объявлений
   var mainPin = map.querySelector(Selector.MAIN_PIN); // блок с меткой
   var form = document.querySelector(Selector.FORM); // блок с формой
@@ -35,27 +37,25 @@
    * @return {object} объект с координатами метки
    */
   function checkPinPosition() {
-    var pinLeft = mainPin.offsetLeft;
-    var pinTop = mainPin.offsetTop;
-    var pinWidth = mainPin.offsetWidth;
-    var pinHeight = mainPin.offsetHeight;
+    var pinX = mainPin.offsetLeft;
+    var pinY = mainPin.offsetTop;
     var mapWidth = map.offsetWidth;
     var coords = {};
 
-    if (pinLeft < 0) {
-      pinLeft = 0;
+    if (pinX < 0) {
+      pinX = 0;
     }
-    if (pinLeft > mapWidth - pinWidth) {
-      pinLeft = mapWidth - pinWidth;
+    if (pinX > mapWidth - PinData.WIDTH) {
+      pinX = mapWidth - PinData.WIDTH;
     }
-    if (mainPin.offsetTop < PIN_Y_MIN - pinHeight) {
-      pinTop = PIN_Y_MIN - pinHeight;
+    if (pinY < PinData.Y_MIN - PinData.HEIGHT) {
+      pinY = PinData.Y_MIN - PinData.HEIGHT;
     }
-    if (pinTop > PIN_Y_MAX - pinHeight) {
-      pinTop = PIN_Y_MAX - pinHeight;
+    if (pinY > PinData.Y_MAX - PinData.HEIGHT) {
+      pinY = PinData.Y_MAX - PinData.HEIGHT;
     }
-    coords.left = pinLeft;
-    coords.top = pinTop;
+    coords.left = pinX;
+    coords.top = pinY;
     return coords;
   }
   /**
@@ -75,8 +75,8 @@
      */
     function onMouseMove(moveEvt) {
       var newCoords = checkPinPosition();
-      var mainPinLeft = newCoords.left;
-      var mainPinTop = newCoords.top;
+      var pinX = newCoords.left;
+      var pinY = newCoords.top;
       var shift = {
         x: startCoords.x - moveEvt.clientX,
         y: startCoords.y - moveEvt.clientY
@@ -86,11 +86,11 @@
         x: moveEvt.clientX,
         y: moveEvt.clientY
       };
-      mainPinTop = (mainPinTop - shift.y);
-      mainPinLeft = (mainPinLeft - shift.x);
+      pinY = (pinY - shift.y);
+      pinX = (pinX - shift.x);
 
-      mainPin.style.left = mainPinLeft + 'px';
-      mainPin.style.top = mainPinTop + 'px';
+      mainPin.style.left = pinX + 'px';
+      mainPin.style.top = pinY + 'px';
     }
     /**
      * обработчик события mouseup (для метки)
@@ -99,7 +99,7 @@
      */
     function onMouseUp(upEvt) {
       upEvt.preventDefault();
-      address.value = window.util.getCoordinates(mainPin, mainPin.offsetWidth / 2, mainPin.offsetHeight);
+      address.value = window.util.getCoordinates(mainPin, PinData.WIDTH / 2, PinData.HEIGHT);
       document.removeEventListener('mousemove', onMouseMove);
       document.removeEventListener('mouseup', onMouseUp);
     }
@@ -117,32 +117,32 @@
    */
   function onMainPinKeydown(evt) {
     var newCoords = checkPinPosition();
-    var mainPinLeft = newCoords.left;
-    var mainPinTop = newCoords.top;
+    var pinX = newCoords.left;
+    var pinY = newCoords.top;
     var keyCode = evt.keyCode;
 
     evt.preventDefault();
 
     if (evt.target === mainPin) {
       if (keyCode === code.LEFT || keyCode === code.A) {
-        mainPinLeft -= MOVE_STEP;
+        pinX -= PinData.MOVE_STEP;
       }
       if (keyCode === code.RIGHT || keyCode === code.D) {
-        mainPinLeft += MOVE_STEP;
+        pinX += PinData.MOVE_STEP;
       }
-      if ((keyCode === code.UP || keyCode === code.W) && ((mainPinTop + mainPin.offsetHeight) > PIN_Y_MIN)) {
-        mainPinTop -= MOVE_STEP;
+      if ((keyCode === code.UP || keyCode === code.W) && (pinY + PinData.HEIGHT) > PinData.Y_MIN) {
+        pinY -= PinData.MOVE_STEP;
       }
-      if ((keyCode === code.DOWN || keyCode === code.S) && ((mainPinTop + mainPin.offsetHeight) < PIN_Y_MAX)) {
-        mainPinTop += MOVE_STEP;
+      if ((keyCode === code.DOWN || keyCode === code.S) && (pinY + PinData.HEIGHT) < PinData.Y_MAX) {
+        pinY += PinData.MOVE_STEP;
       }
 
       if (map.classList.contains(ClassName.MAP_FADED)) {
         window.map.activate();
       }
-      mainPin.style.left = mainPinLeft + 'px';
-      mainPin.style.top = mainPinTop + 'px';
-      address.value = window.util.getCoordinates(mainPin, mainPin.offsetWidth / 2, mainPin.offsetHeight);
+      mainPin.style.left = pinX + 'px';
+      mainPin.style.top = pinY + 'px';
+      address.value = window.util.getCoordinates(mainPin, PinData.WIDTH / 2, PinData.HEIGHT);
     }
   }
   /**
@@ -158,5 +158,4 @@
   mainPin.addEventListener('mousedown', onMainPinMouseDown);
   mainPin.addEventListener('keydown', onMainPinKeydown);
   mainPin.addEventListener('keydown', onMainPinTabPress);
-
 })();
